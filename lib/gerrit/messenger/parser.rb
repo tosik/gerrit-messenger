a695ff39fb37c04ec05cfd06e860c045cb0d0035
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2013-06-02 18:31:34 +0900

    Replaces to new parser spec

--- lib/gerrit/messenger/parser.rb
+++ lib/gerrit/messenger/parser.rb
@@ -7,14 +7,22 @@ module Gerrit
       module_function
       def parse(string)
         json = JSON.parse(string)
-        if (json["type"] == 'comment-added')
+        case(json["type"])
+        when 'comment-added'
           create_comment_added_message(json)
+        when 'patchset-created'
+          create_patchset_created_message(json)
         end
       end
 
       def create_comment_added_message(json)
         parsed = Gerrit::Messenger::Models::CommentAdded.new(json)
         "[COMMENT][#{parsed.score}] #{parsed.owner_name} -> #{parsed.author_name} \"#{parsed.subject}\" #{parsed.url}"
+      end
+
+      def create_patchset_created_message(json)
+        parsed = Gerrit::Messenger::Models::PatchsetCreated.new(json)
+        "[NEW PATCHSET] #{parsed.owner_name} \"#{parsed.subject}\" #{parsed.url}"
       end
 
     end
